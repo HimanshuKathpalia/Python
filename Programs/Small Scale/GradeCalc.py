3c94650290c907c3b7d282c3fb5b44720a2a8ffe
HimanshuKathpalia <36640535+HimanshuKathpalia@users.noreply.github.com>
2023-04-26 12:49:05 +0530

    Update GradeCalc.py

--- Programs/Small Scale/GradeCalc.py
+++ Programs/Small Scale/GradeCalc.py
@@ -8,17 +8,26 @@ Score Grade
 < 0.6 F
 If the user enters a value out of range, print a suitable error message and exit. For the test, enter a score of 0.85.'''
 
+**************************************************************************************************************************
 
+# Prompt user for input of score
 score = float(input("Enter Score: "))
+
+# Check if score is out of range
 if(score<0.0 or score>1.0):
     print("Error")
+# Check if score is A grade or higher
 elif(score>= 0.9):
     print("A")
+# Check if score is B grade or higher
 elif(score>= 0.8):
     print("B")
+# Check if score is C grade or higher
 elif(score>= 0.7):
     print("C")
+# Check if score is D grade or higher
 elif(score>= 0.6):
     print("D")
+# If score is lower than D grade, print F grade
 else:
     print("F")
